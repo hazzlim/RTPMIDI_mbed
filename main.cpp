07353a60124ddfad37f1cde5436944024b0bc006
Hari Limaye <hari.limaye@arm.com>
2021-07-26 10:46:21 +0100

    Get accelerometer data and transform into pitch bend value

--- main.cpp
+++ main.cpp
@@ -4,10 +4,33 @@
  */
 
 #include "mbed.h"
+#include "fxos8700cq.h"
 
+#define I2C0_SDA PTD9
+#define I2C0_SCL PTD8
+
+FXOS8700CQ acc(I2C0_SDA, I2C0_SCL);
+
+int modulate(Data &values)
+{
+    return (0x3FFF * (values.ax + 1)) / 2;
+}
 
 int main()
 {
-    printf("Hello, Mbed!\n");
-    return 0;
+    acc.init();
+
+    while (1) {
+
+        // poll the sensor and get the values, storing in a struct
+        Data values = acc.get_values();
+
+        // generate the bend value from the sensor values
+        int bend = modulate(values);
+
+        // print the bend value
+        printf("Pitch bend value: %d\n", bend);
+
+        ThisThread::sleep_for(500ms);
+    }
 }
